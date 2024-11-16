3631490f922fb6e0cdad6c0f5ce428268c6f2901
Fatma Osama <fosama1570@gmail.com>
2024-11-16 22:27:33 +0200

    Created Tables  1,5,9,13,17,21

--- DatbaseProjectM2/Schema.sql
+++ DatbaseProjectM2/Schema.sql
@@ -1,17 +1,168 @@
 -- Write your own SQL object definition here, and it'll be included in your package.
-CREATE DATABASE CAT
-CREATE TABLE cat (
-    StudentID INT PRIMARY KEY,
-    FirstName VARCHAR(50),
-    LastName VARCHAR(50),
-    EnrollmentDate DATE
+CREATE DATABASE Project
+
+-- 1 Creation of Learner
+CREATE TABLE Learner(
+    int LearnerID PRIMARY KEY IDENTITY,
+    first_name VARCHAR(20),
+    last_name VARCHAR(20),
+    gender VARCHAR(20),
+    birth_date DATE,
+    country VARCHAR(20),
+    cultural_background VARCHAR(20)
 );
 
--- Insert data into the table
-INSERT INTO cat (StudentID, FirstName, LastName, EnrollmentDate)
-VALUES
-(1, 'John', 'Doe', '2024-01-15'),
-(2, 'Jane', 'Smith', '2024-02-20');
+-- 1 Learners multivaled attributes
+CREATE TABLE Skills(
+   int LearnerID PRIMARY KEY IDENTITY,
+   skill VARCHAR(20),
+   PRIMARY KEY (LearnerID, skill),
+   FOREIGN KEY (LearnerID) REFERENCES Learner(LearnerID) ON DELETE CASCADE ON UPDATE CASCADE 
+);
 
--- Retrieve data from the table
-SELECT * FROM cat;
+CREATE TABLE LearningPreference(
+   int LearnerID PRIMARY KEY IDENTITY,
+   preference VARCHAR(20),
+   PRIMARY KEY (LearnerID, preference),
+   FOREIGN KEY (LearnerID) REFERENCES Learner(LearnerID) ON DELETE CASCADE ON UPDATE CASCADE 
+);
+
+-- 2 Creation of Personalization Profiles
+    --Enter code here
+
+--3 Creation of Courses
+    --Enter code here
+
+--4 Creation of Modules
+    --Enter code here
+
+--5 Creation of Learning Activities
+    CREATE TABLE Learning_activities(
+        int ActivityID PRIMARY KEY IDENTITY,
+        activity_type VARCHAR(20),
+        instruction_details VARCHAR(20),
+        Max_points INT,
+        ModuleID INT,
+        FOREIGN KEY (ModuleID) REFERENCES Modules(ModuleID) ON DELETE CASCADE ON UPDATE CASCADE ,
+        CourseID INT,
+        FOREIGN KEY (CourseID) REFERENCES Course(CourseID) ON DELETE CASCADE ON UPDATE CASCADE 
+    );
+
+--6 Creation of Course Enrollment
+    --Enter code here
+
+--7 Creation of Achievements
+    --Enter code here
+
+--8 Creation of Badges
+    --Enter code here
+
+--9 Creation of Leaderboards
+    CREATE TABLE Leaderboard(
+        int BoardID PRIMARY KEY IDENTITY,
+        season VARCHAR(20)
+    );
+    
+
+--10 Creation of Learning Paths
+    --Enter code here
+
+--11 Creation of Interaction Logs
+    --Enter code here
+
+--12 Creation of Emotional Feedback
+    --Enter code here
+
+--13 Creation of Quest
+     CREATE TABLE Quest(
+        int QuestID PRIMARY KEY IDENTITY,
+        difficulty_level VARCHAR(20),
+        criteria VARCHAR(20),
+        Qdescription VARCHAR(20), --I added the Q as description is a keyword
+        title VARCHAR(20),
+     );
+
+-- 13 Quest Inheritance
+    CREATE TABLE Skill_Mastery(
+         int QuestID PRIMARY KEY IDENTITY,
+         skill VARCHAR(20),
+         PRIMARY KEY (QuestID, skill),
+         FOREIGN KEY (QuestID) REFERENCES Quest ON DELETE CASCADE ON UPDATE CASCADE 
+    );
+
+    CREATE TABLE Collaborative(
+         int QuestID PRIMARY KEY IDENTITY,
+         deadline DATE,
+         max_num_participants INT,
+          FOREIGN KEY (QuestID) REFERENCES Quest ON DELETE CASCADE ON UPDATE CASCADE 
+    );
+
+--14 Creation of Rewards
+    --Enter code here
+
+--15 Creation of Skill Progression
+    --Enter code here
+
+--16 Creation of Assesments
+    --Enter code here
+
+--17 Creation of Instructor
+    CREATE TABLE Instructor(
+        int InstructorID PRIMARY KEY IDENTITY,
+        InstructorName VARCHAR(20), --Cahnged to InstructorName as name is a keyword
+        latest_qualification VARCHAR(20),
+        expertise_area VARCHAR(20),
+        email VARCHAR(20)
+    );
+
+--17 Relation between the LearningPath and Instructor
+    CREATE TABLE Pathreview(
+        InstructorID INT,
+        PathID INT, 
+        Feedback VARCHAR(20),
+        PRIMARY KEY (InstructorID, PathID),
+        FOREIGN KEY (InstructorID) REFERENCES Instructor ON DELETE CASCADE ON UPDATE CASCADE,
+        FOREIGN KEY (PathID) REFERENCES Learning_path ON DELETE CASCADE ON UPDATE CASCADE 
+    );
+
+--17 Relation between Emotional Feedback and Instructor
+    CREATE TABLE Emotionalfeedback_review(
+        FeedbackID INT,
+        InstructorID INT,
+        feedback VARCHAR(20),
+        PRIMARY KEY (FeedbackID, InstructorID),
+        FOREIGN KEY (FeedbackID) REFERENCES Emotional_feedback ON DELETE CASCADE ON UPDATE CASCADE,
+        FOREIGN KEY (InstructorID) REFERENCES Instructor ON DELETE CASCADE ON UPDATE CASCADE
+    );
+
+--18 Creation of Content Library
+    --Enter code here
+
+--19 Creation of Notifications
+    --Enter code here
+
+--20 Creation of Disscusion Forums
+    --Enter code here
+
+--21 Creation of Learning Goals
+    CREATE TABLE Learning_goal(
+         int ID PRIMARY KEY IDENTITY,
+         status VARCHAR(20),
+         deadline DATE,
+         Gdescription VARCHAR(20) --Wrote Gdescription instead of description(keyword)
+    );
+
+--21 Relation between Learning goals and Learner
+    CREATE TABLE LearnersGoals(
+        GoalID INT,
+        LearnerID INT,
+        PRIMARY KEY (GoalID, LearnerID),
+        FOREIGN KEY (GoalID) REFERENCES Learning_goal ON DELETE CASCADE ON UPDATE CASCADE,
+        FOREIGN KEY (LearnerID) REFERENCES Learner ON DELETE CASCADE ON UPDATE CASCADE
+    );
+
+--22 Creation of Surveys
+    --Enter code here
+
+
+
